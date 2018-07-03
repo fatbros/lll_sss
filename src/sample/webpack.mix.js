ef58d28e6e5e20161c3ef40f910b156ca2ec0e5c
kondo-shun <kondo-shun@dmm.com>
2018-07-03 11:43:39 +0900

    laravelにReactを導入, route controllerの設定

--- src/sample/webpack.mix.js
+++ src/sample/webpack.mix.js
@@ -11,5 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.react('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.react('resources/assets/js/components/Example.js', 'public/js/app.js');
